74f881a7a2d0ce448e1991b62d86e4ca743f7794
reelbeelveel <k.belveal17@gmail.com>
2023-01-03 16:23:39 -0500

    removed password from code

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,16 @@
 #include <BluetoothSerial.h>
 #include <EELSP.h>
 
-#define SSID       "Nighthawk"
-#define PASSWD     "Attheendoftheday"
-#define STATIC_IP  IPAddress(192, 168, 1, 32)
-#define GATEWAY_IP IPAddress(192, 168, 1, 1)
-#define SUBNET_IP  IPAddress(255, 255, 255, 0)
-
 #define drawPixel(a, b, c)            \
     M5.Lcd.setAddrWindow(a, b, a, b); \
     M5.Lcd.pushColor(c)
 
 bool isConnected = false;
 
-void initializeWiFi() {
-    WiFi.mode(WIFI_STA);
-    WiFi.config(STATIC_IP, GATEWAY_IP, SUBNET_IP);
-    WiFi.begin(SSID, PASSWD);
-}
-
 void setup() {
   // put your setup code here, to run once:
   M5.begin();
   M5.Lcd.setRotation(1);
-  initializeWiFi();
 }
 
 void loop() {
@@ -42,7 +29,6 @@ void loop() {
     M5.Lcd.setCursor(0, 0);
     M5.Lcd.setTextColor(WHITE);
     M5.Lcd.setTextSize(2);
-    M5.Lcd.printf("Connected to %s", SSID);
   }
 
   if(M5.BtnA.isPressed()) {
